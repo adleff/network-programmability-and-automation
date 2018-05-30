96460097f98cfea1b0a957331906965f0e8b185c
adleff <adamleff@gmail.com>
2018-05-30 10:38:10 -0700

    more j2 tweaking

--- jinja1.py
+++ jinja1.py
@@ -1,6 +1,6 @@
 from jinja2 import Environment, FileSystemLoader
 
-ENV = Environment(loader=FileSystemLoader('.'))
+ENV = Environment(lstrip_blocks=True, trim_blocks=True,keep_trailing_newline=False, loader=FileSystemLoader('.'))
 template = ENV.get_template("template1.j2")
 
 class NetworkInterface(object):
