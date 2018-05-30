ff003c4858e732ab11460a88e4b7bcc8ac2b9eb1
adleff <adamleff@gmail.com>
2018-05-30 13:22:33 -0700

    removed classes

--- jinja1.py
+++ jinja1.py
@@ -1,16 +1,8 @@
 from jinja2 import Environment, FileSystemLoader
 
-ENV = Environment(lstrip_blocks=True, trim_blocks=True,keep_trailing_newline=False, loader=FileSystemLoader('.'))
+ENV = Environment(lstrip_blocks=True, trim_blocks=True, loader=FileSystemLoader('.'))
 template = ENV.get_template("template1.j2")
 
-class NetworkInterface(object):
-
-    def __init__(self, name, description, vlan, uplink=False):
-        self.name = name
-        self.description = description
-        self.vlan = vlan
-        self.uplink = uplink
-
-interface_obj = NetworkInterface("GigabitEthernet0/1", "Server Port", 10)
+interface_obj = {'name': 'GigabitEthernet0/1', 'vlan': 10, 'description': 'SERVER', 'uplink': False}
 
 print(template.render(interface=interface_obj))
